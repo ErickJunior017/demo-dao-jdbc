25bae3cea505fe7c8f7bf534c2ce9c875fff75da
Erick Junior <erickjuniorbatistagarcia2@gmail.com>
2024-11-02 23:34:54 -0300

    FindById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,12 +11,10 @@ public class Program {
 
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "Books");
-        Seller seller = new Seller(32, "Bobby", "bob@gmail.com", new Date(), 3000.0, obj);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        System.out.println(obj);
+        Seller seller = sellerDao.findById(1);
+
         System.out.println(seller);
 
     }
